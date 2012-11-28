c32858ec107f38a64fe2968392ac446128655109
Meikel <meikel@meikel.at>
2012-11-28 23:40:26 +0100

    Configure maven-assembly-plugin to create jar-with-dependencies.

--- src/samples/sample.bat
+++ src/samples/sample.bat
@@ -1,5 +1,5 @@
 @echo off
 
-java -cp ..\..\target\at.meikel.util.xlsconverter-0.0.1-SNAPSHOT.jar at.meikel.util.excelconverter.ExcelConverterMain Mappe1.xls Mappe1.txt
+java -cp ..\..\target\at.meikel.util.xlsconverter-0.0.1-SNAPSHOT-jar-with-dependencies.jar at.meikel.util.excelconverter.ExcelConverterMain Mappe1.xls Mappe1.txt
 
 pause
